a8e8fdc1840e5c30f72d87167aeee0132e7bfa2b
mojo <moran.brix@gmail.com>
2018-06-14 12:38:10 +0300

    Delete / creat /read

--- src/com/luv2code/hibernate/demo/entity/Student.java
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -2,6 +2,8 @@ package com.luv2code.hibernate.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,8 +12,9 @@ import javax.persistence.Table;
 public class Student {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
-	private int id;
+	private Integer id;
 	@Column(name="first_name")
 	private String firstName;
 	@Column(name="last_name")
@@ -21,17 +24,22 @@ public class Student {
 	
 	public Student() {}
 
+	
+
 	public Student(String firstName, String lastName, String email) {
+		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
 
-	public int getId() {
+
+
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
